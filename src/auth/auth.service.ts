f42a18f2a75acd874b0d34b126ea3a7af268894b
BorovskovaYevheniia <borovskova.yevheniia@lazy-ants.net>
2023-05-07 21:24:19 +0300

    added e2e tests with package pactumJs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,7 +7,7 @@ import { ConfigService } from '@nestjs/config';
 
 import * as argon from 'argon2';
 
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 
 @Injectable()
